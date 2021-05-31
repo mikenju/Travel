156ef2352a24a5dba16d1f6792aec6f6e37e2cf7
Ra4inA <vitya-sorokin-92@mail.ru>
2021-05-31 18:56:30 +0300

    Анимации
    Добавил красивый эффект анимации при скролле. Выполнено с помощью библиотеки JQuery, без плагинов

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
-// $(document).ready(function(){
-// 	$('.slider').slick({
-// 		touchThreshold: 10,
-// 		speed: 300,
-// 	});
-// });
-
-
-
-// $(function(){
-//   $(".slider").on('afterChange', function(event, slick, currentSlide){
-//      $("#cp").text(currentSlide + 1);
-//   });
-// });
-
-
 $(function(){
   $('.slider').slick({    
     // autoplay: true,
@@ -27,3 +11,19 @@ $(function(){
      $("#cp").text('0' + (currentSlide + 1));
   });
 });
+
+ $(".book ._animate").addClass("_anim-no-hide");
+ $("h3._animate, h4._animate").addClass("_anim-no-hide");
+ $("tour__text p").addClass("_animate _animate-right");
+ const animItems = document.querySelectorAll("._animate");
+ $(window).scrollTop(1);
+ $(window).on("scroll", function(){
+   for (let index = 0; index < animItems.length; index++){
+     if($(window).scrollTop() + $(window).height() > $(animItems[index]).offset().top + $(animItems[index]).height() / 2 
+       & $(window).scrollTop() < $(animItems[index]).offset().top + $(animItems[index]).height() / 2){
+       $(animItems[index]).addClass("_animate-Active");
+     } else {
+       if($(animItems[index]).hasClass("_anim-no-hide") !== true){$(animItems[index]).removeClass("_animate-Active");}
+     }
+   }
+ });
